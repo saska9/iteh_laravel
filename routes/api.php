d04612504e22bfa8d3c20f58cc1ff0338ea9a647
saska9 <abrankovic862@gmail.com>
2023-09-04 17:27:13 +0200

    finalna verzija

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\DirectorController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\MovieDirectorController;
+use App\Http\Controllers\UserMovieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +21,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('/users', [UserController::class,'index']);
+Route::get('/users/{id}', [UserController::class,'show']);
+
+Route::resource('movies', MovieController::class)->only(['index','show']);
+Route::resource('users.movies', UserMovieController::class);
+
+Route::resource('director',DirectorController::class);
+Route::resource('genre',GenreController::class);
+
+Route::get('/users/{id}/movies',[UserMovieController::class, 'index']);
+Route::resource('directors/{id}/movies',MovieDirectorController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware'=>['auth:sanctum']], function(){
+    Route::get('/profile', function(Request $request){
+        return auth()->user();
+    });
+    Route::resource('movies', FilmController::class)->only(['update','store','destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
